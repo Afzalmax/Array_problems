99e896583b51d8bbbf8b1135e4adde0ec33d424c
Afzalmax <rafzal0606@gmail.com>
2024-08-03 16:24:11 +0530

    coin change

---
+++ Leetcode-coinchange.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int x = coin; x <= amount; x++) {
+                dp[x] = Math.min(dp[x], dp[x - coin] + 1);
+            }
+        }
+
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
